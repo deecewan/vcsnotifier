9569edc9d55f03e79fce4c1e0994ac81961832ce
David Buchan-Swanson <david.buchanswanson@gmail.com>
2017-06-19 22:03:15 +1000

    Initial Commit

---
+++ GithubNotifier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GithubNotifier
+//
+//  Created by David Buchan-Swanson on 19/6/17.
+//  Copyright © 2017 David Buchan-Swanson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GithubNotifier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GithubNotifier
+//
+//  Created by David Buchan-Swanson on 19/6/17.
+//  Copyright © 2017 David Buchan-Swanson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
