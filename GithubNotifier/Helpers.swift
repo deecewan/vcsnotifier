71c124acf5b84bd5f2560527132ce566c9fc1c5a
David Buchan-Swanson <david.buchanswanson@gmail.com>
2017-06-26 22:17:13 +1000

    re-write menu helpers

---
+++ GithubNotifier/Helpers.swift
@@ -0,0 +1,50 @@
+//
+// Created by David Buchan-Swanson on 26/6/17.
+// Copyright (c) 2017 David Buchan-Swanson. All rights reserved.
+//
+
+import Foundation
+
+extension StatusMenuController {
+  func checkForAPIKey() {
+    // check if the api key exists
+    // if it doesn't, add a notification
+  }
+
+  func extractCommentIdUrl(_ url: String?) -> String {
+    if url == nil {
+      return ""
+    }
+    let url = url!
+    let splits = url.characters.split(separator: "/").map{item in
+      String(item)
+    }
+    let secondLast = splits[splits.endIndex - 2]
+    if secondLast != "comments" {
+      return ""
+    }
+
+    let type = splits[splits.endIndex - 3]
+    let id = splits[splits.endIndex - 1]
+
+    switch(type) {
+      case "pulls":
+        return "#discussion_r\(id)"
+      case "issues":
+        return "#issuecomment-\(id)"
+      default:
+        return "#commitcomment-\(id)"
+    }
+  }
+
+  func normaliseUrl(_ url: String, extra: String) -> String {
+    // remove all the incorrect things
+    // replace all wrong plurals
+    let newUrl = url.replacingFirst(matching: "api.", with: "")
+        .replacingFirst(matching: "repos/", with: "")
+        .replacingFirst(matching: "pulls/", with: "pull/")
+        .replacingFirst(matching: "commits/", with: "commit")
+
+    return "\(newUrl)\(extra)"
+  }
+}
